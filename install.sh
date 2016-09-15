046e1ed5b2d9e6345492699795f578bae687e44e
Harold Schreckengost <harold@haroldmschreckengost.com>
2016-09-15 07:56:44 -0500

    Added modules, linking

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 I3_CONF=$HOME/.config/i3/config
 I3_BLOCKS_CONF=$HOME/.i3blocks.conf
+I3_BLOCKS_DIR=$HOME/.i3blocks
 
-cp config $I3_CONF
-cp i3blocks.conf $I3_BLOCKS_CONF
+ln -s $PWD/config $I3_CONF
+ln -s $PWD/i3blocks.conf $I3_BLOCKS_CONF
+ln -s $PWD/i3blocks $I3_BLOCKS_DIR 
