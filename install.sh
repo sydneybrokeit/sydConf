904c0cb3505d06b358d755ddb6e999878a7ea418
Harold Schreckengost <harold@haroldmschreckengost.com>
2016-09-17 17:50:39 -0500

    Termite Config, changes to the installw

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 I3_CONF=$HOME/.config/i3/config
 I3_BLOCKS_CONF=$HOME/.i3blocks.conf
 I3_BLOCKS_DIR=$HOME/.i3blocks
-
-ln -s $PWD/config $I3_CONF
+TERMITE_CONFIG=$HOME/.config/termite/config
+ln -s $PWD/i3config $I3_CONF
 ln -s $PWD/i3blocks.conf $I3_BLOCKS_CONF
 ln -s $PWD/i3blocks $I3_BLOCKS_DIR 
+ln -s $PWD/termite.config $TERMITE_CONFIG
