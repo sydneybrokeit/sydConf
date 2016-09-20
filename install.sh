5a693ec2b5b1d7b31d53d7b8679cddd703f42e26
Harold Schreckengost <harold@haroldmschreckengost.com>
2016-09-19 19:52:46 -0500

    VIM!

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@ I3_CONF=$HOME/.config/i3/config
 I3_BLOCKS_CONF=$HOME/.i3blocks.conf
 I3_BLOCKS_DIR=$HOME/.i3blocks
 TERMITE_CONFIG=$HOME/.config/termite/config
+VIMRC_LOC=$HOME/.vimrc
+
 ln -s $PWD/i3config $I3_CONF
 ln -s $PWD/i3blocks.conf $I3_BLOCKS_CONF
 ln -s $PWD/i3blocks $I3_BLOCKS_DIR 
 ln -s $PWD/termite.config $TERMITE_CONFIG
+ln -s $PWD/vimrc $VIMRC_LOC
