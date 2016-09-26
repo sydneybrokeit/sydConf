9b92fde8e0276bacaa51ee60890df29251943dfa
Harold Schreckengost <harold@haroldmschreckengost.com>
2016-09-26 12:30:46 -0500

    Resources

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ I3_BLOCKS_DIR=$HOME/.i3blocks
 TERMITE_CONFIG=$HOME/.config/termite/config
 VIMRC_LOC=$HOME/.vimrc
 VIM_DIR=$HOME/.vim
+Xresources_FILE=$HOME/.Xresources
 
 ln -sf $PWD/i3config $I3_CONF
 ln -sf $PWD/i3blocks.conf $I3_BLOCKS_CONF
@@ -11,3 +12,4 @@ ln -sf $PWD/i3blocks $I3_BLOCKS_DIR
 ln -sf $PWD/termite.config $TERMITE_CONFIG
 ln -sf $PWD/vimrc $VIMRC_LOC
 ln -sf $PWD/vim/ $VIM_DIR
+ln -sf $PWD/Xresources $Xresources_FILE
